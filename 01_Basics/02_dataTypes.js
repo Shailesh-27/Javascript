9648e967bdb6f08065bb7d439de0e47882802432
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-11 06:44:44 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert("Hello")  // We are using nodejs,not browser
+
+let name = "Shailesh"
+let age = 22
+let isLoggedIn = true
+
+
+// number  => 2^53
+// bigInt
+// string
+// bool => true/false
+// null => standalone value
+// undefined 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "Shailesh")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
+
+
+
+
+
