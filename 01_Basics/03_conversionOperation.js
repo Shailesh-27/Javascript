275b424a58d172bf3d28f9e912994d21773d62bb
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-11 07:47:36 +0000

    Conversion operation 2

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
-let score = "Shailesh"
+// let score = "Shailesh"
 
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
 // "33" => 33
@@ -15,19 +15,50 @@ console.log(valueInNumber)
 // true => 1
 // false => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true
 // 0 => false
 // "" => false
 // "Shailesh" => true
 
-let sumNumber = 33
+// let sumNumber = 33
 
-let stringNumber = String(sumNumber)
+// let stringNumber = String(sumNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// **************************** Operations ****************************
+
+let value = 3
+let negValue = -value
+
+let str1 = "Hello"
+let str2 = " Shailesh"
+let str3 = str1 + str2
+
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+
+++gameCounter
+console.log(gameCounter);
+
+
+
+
