5401d12e03aaf40e0d3422794480da44cf9db896
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-11 06:58:14 +0000

    Conversion of datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Shailesh"
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Shailesh" => true
+
+let sumNumber = 33
+
+let stringNumber = String(sumNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
