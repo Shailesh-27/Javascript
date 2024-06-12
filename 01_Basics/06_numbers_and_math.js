4bd78074260ad94cd7598afead3cb5d92f8ed220
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-12 06:38:20 +0000

    Numbers and Maths

---
+++ 01_Basics/06_numbers_and_math.js
@@ -0,0 +1,49 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(10000)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNum = 234.9866
+// console.log(otherNum.toPrecision(4));
+
+// const num2 = 100000
+// console.log(num2.toLocaleString('en-IN'));
+
+
+// *********************** Maths ************************
+
+console.log(Math.PI)
+
+console.log(Math.abs(-6));
+
+console.log(Math.round(5.4));
+console.log(Math.ceil(5.4));
+console.log(Math.floor(5.4));
+
+console.log(Math.min(1,4,0));
+console.log(Math.max(1,4,0));
+
+console.log(Math.random());
+console.log((Math.random() * 10 )+ 1);
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
+
+
+
+
+
+
+
+
+
+
+
+
