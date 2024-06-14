0e5d7f2edba5c02944b1ddb687c6648e75f465dd
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-14 08:17:52 +0000

    Learn more about scopes

--- 03_Basics/03_scopes.js
+++ 03_Basics/03_scopes.js
@@ -5,10 +5,52 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    console.log("Innner: ",a);
+    // console.log("Innner: ",a);
 
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// Nested scopes
+
+function one(){
+    const username = "sam"
+
+    function two(){
+        const website = "google"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+
+// one()
+
+if(true){
+    const username = "sam"
+    if(username === 'sam'){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    //  console.log(website);
+}
+
+// console.log(username);
+
+// ******************* intresting *********************
+
+function addOne(value){
+    return value + 1
+}
+
+console.log(addOne(6))
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(6)
