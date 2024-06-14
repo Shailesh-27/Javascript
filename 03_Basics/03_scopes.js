cfd12051dc5de526bf3bf0b37262c4dc62ce51b9
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-14 08:00:11 +0000

    Learn about scopes => Global and Local

---
+++ 03_Basics/03_scopes.js
@@ -0,0 +1,14 @@
+// var c = 300
+let a = 100
+
+if(true){
+    let a = 10
+    const b = 20
+    // var c = 30
+    console.log("Innner: ",a);
+
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
