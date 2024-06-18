fb42e2a56516bbd3c432aa3f62fa0193d9ac5cd4
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-18 14:14:06 +0000

    for in loop

---
+++ 05_iteration/04_for_in_loop.js
@@ -0,0 +1,28 @@
+const myObj = {
+    JS: 'JavaScript',
+    cpp: 'C++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+
+for (const key in myObj) {
+    // console.log(key, ':-',myObj[key]);
+}
+
+// for array
+
+const programming = ['JS','HTML','rb','py','dart']
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+// const newMap = new Map()
+// newMap.set('IN','India')
+// newMap.set('US','USA')
+// newMap.set('FR','France')
+
+// for (const key in newMap) {
+//     console.log(key);
+// }
+
