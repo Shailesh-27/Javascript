06dc877165521c72a252be25952e8e8914fdf542
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-18 13:39:05 +0000

    while and do while loop

---
+++ 05_iteration/02_while_loop.js
@@ -0,0 +1,26 @@
+
+let index = 0
+
+while (index <= 10) {
+    // console.log(`Value of i is ${index}`);
+    index += 2
+}
+
+const myArray = ['flash','batman', 'superman']
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(myArray[arr]);
+    arr++
+    
+}
+
+// Do while loop
+
+let score = 11
+
+
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <= 10);
