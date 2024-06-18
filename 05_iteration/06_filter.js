ebdeb5bc7abc3fc9aa75bc65862361ea29747ba6
Shailesh Prajapati <59478408+Shailesh-27@users.noreply.github.com>
2024-06-18 14:49:04 +0000

    Learn about filter

---
+++ 05_iteration/06_filter.js
@@ -0,0 +1,29 @@
+const coding = ['JS','python','rb','dart','java']
+
+const values =  coding.forEach((val) => {
+    // console.log(val);
+})
+
+// console.log(values);
+
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const result = myNums.filter((num ) => num > 4)
+// const result = myNums.filter((num ) => {
+//     return num > 4
+// })
+
+// console.log(result);
+
+// const myNum  = []
+
+// myNums.forEach((item) => {
+//     if(item > 4){
+//         myNum.push(item)
+//     }
+// })
+
+// console.log(myNum);
+
